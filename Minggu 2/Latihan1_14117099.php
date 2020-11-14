bd18dee85add065ed9715f47c2e7941dc4d77246
YOGANAUFALRP <yoganaufal55@gmail.com>
2020-11-14 15:27:46 +0700

    Update
    Minggu 2

---
+++ Minggu 2/Latihan1_14117099.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan1</title>
+</head>
+
+<body>
+    <h1>PROGRAM KALKULATOR</h1>
+    
+    <form action = "form_get.php" method="get">
+    Nilai 1 : <input type ="number" Name="nilai1"  required=""> 
+    <br>
+    Nilai 2 : <input type ="number" Name="nilai2" required="">
+    <br>
+    <input type = "submit" href = "form_get.php">
+    </form>
+
+</body>
+</html>
